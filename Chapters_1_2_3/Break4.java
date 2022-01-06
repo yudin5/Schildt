da7992d4db02463edc075355a37d958abee9b7e1
yudin5 <yudin5@yandex.ru>
2022-01-06 10:12:23 +0300

    refactoring

--- Chapters_1_2_3/Break4.java
+++ Chapters_1_2_3/Break4.java
@@ -1,17 +1,20 @@
 package Chapters_1_2_3;// Using break with a  label.
 
 class Break4 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int i;
 
-        for(i=1; i<4; i++) {
-            one:  {
-                two:    {
-                    three:    {
+        for (i = 1; i < 4; i++) {
+            one:
+            {
+                two:
+                {
+                    three:
+                    {
                         System.out.println("\ni is " + i);
-                        if(i==1) break one;
-                        if(i==2) break two;
-                        if(i==3) break three;
+                        if (i == 1) break one;
+                        if (i == 2) break two;
+                        if (i == 3) break three;
 
                         // this is never reached
                         System.out.println("won't print");
